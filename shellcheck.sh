90d02914432dd3922d1fc6c84ac358191f20d3bb
hayao <shun819.mail@gmail.com>
2020-12-27 09:17:01 +0900

    [fix] : FIxed script status

--- shellcheck.sh
+++ shellcheck.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -eu
+
 script_path="$( cd -P "$( dirname "$(readlink -f "$0")" )" && pwd )"
 cd "${script_path}"
 
-/usr/bin/ls | grep -E 'alterlinux-*' | xargs -Ifile bash -c 'shopt -s globstar nullglob; shellcheck -s bash --exclude=SC2068 -S error ./file/file'
+ls | grep -E 'alterlinux-*' | xargs -Ifile bash -c 'shopt -s globstar nullglob; shellcheck -s bash --exclude=SC2068 -S error ./file/file'
 bash -c 'shopt -s globstar nullglob; shellcheck -s bash --exclude=SC2068 -S error ./shellcheck.sh'
