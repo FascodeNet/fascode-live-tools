24afe7532293d735d97b6c989ebb61549819a3dc
hayao <hayao@fascode.net>
2021-07-12 21:40:15 +0900

    [add] : Use github workflows instead of traviscl

--- shellcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-script_path="$( cd -P "$( dirname "$(readlink -f "$0")" )" && pwd )"
-cd "${script_path}"
-
-ls | grep -E 'alterlinux-*' | xargs -Ifile bash -c 'shopt -s globstar nullglob; shellcheck -s bash --exclude=SC2068 -S error ./file/file'
-bash -c 'shopt -s globstar nullglob; shellcheck -s bash --exclude=SC2068 -S error ./shellcheck.sh'
